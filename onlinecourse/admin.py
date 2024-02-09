c8618c5a43386a27147551dacc03312ab9d77c2c
michael <michaelalexander6666@outlook.com>
2024-02-09 12:46:47 -0500

    until form submission

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInLine(admin.StackedInline):
+    model = Question
+    extra = 2
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -24,6 +33,9 @@ class LessonAdmin(admin.ModelAdmin):
 
 # <HINT> Register Question and Choice models here
 
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
